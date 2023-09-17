6162a996f8104eb6b573f59e114df03de0df11b6
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-18 00:40:23 +0300

    10.4

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        Product[] products = {
-                new Product("milk", 75),
-                new Product("Butter", 120),
-                new Product("chise", 180),
-                new Product("teapod", 1890),
-                new Product("water filter", 1200)
+        Figure[][] board = new Figure[8][];
+        board[0] = new Figure[]{
+                new Figure(FigureColor.WHITE, FigureType.ROOK),
+                new Figure(FigureColor.WHITE, FigureType.HORSE),
+                new Figure(FigureColor.WHITE, FigureType.ELEPHANT),
+                new Figure(FigureColor.WHITE, FigureType.KING),
+                new Figure(FigureColor.WHITE, FigureType.QUEEN),
+                new Figure(FigureColor.WHITE, FigureType.ELEPHANT),
+                new Figure(FigureColor.WHITE, FigureType.HORSE),
+                new Figure(FigureColor.WHITE, FigureType.ROOK),
         };
-
-        int MIN_PRICE_FOR_DISCOUNT = 1000;
-
-        double discount = 0.1;
-        for(int i = 0; i<products.length; i++){
-            if(products[i].getPrice() >= MIN_PRICE_FOR_DISCOUNT){
-                products[i] = new Product(products[i].getName(), (int)(products[i].getPrice() * (1 - discount)));
-            }
+        board[1] = new Figure[8];
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new Figure(FigureColor.WHITE, FigureType.PAWN);
         }
-
-        for(Product product:products){
-            System.out.println(product);
+        board[6] = new Figure[8];
+        for (int i = 0; i < 8; i++) {
+            board[6][i] = new Figure(FigureColor.BLACK, FigureType.PAWN);
+        }
+        board[7] = new Figure[]{
+                new Figure(FigureColor.BLACK, FigureType.ROOK),
+                new Figure(FigureColor.BLACK, FigureType.HORSE),
+                new Figure(FigureColor.BLACK, FigureType.ELEPHANT),
+                new Figure(FigureColor.BLACK, FigureType.KING),
+                new Figure(FigureColor.BLACK, FigureType.QUEEN),
+                new Figure(FigureColor.BLACK, FigureType.ELEPHANT),
+                new Figure(FigureColor.BLACK, FigureType.HORSE),
+                new Figure(FigureColor.BLACK, FigureType.ROOK),
+        };
+        for (int i = 2; i < 6; i++) {
+            board[i] = new Figure[8];
+        }
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] == null ? "--" : board[i][j]);
+                System.out.print(j < 7 ? " " : "\n");
+            }
         }
     }
 }
