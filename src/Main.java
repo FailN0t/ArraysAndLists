de5f59bce9584b09d32718241445a852f5484dde
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-17 23:34:51 +0300

    10.3

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@ public class Main {
     public static void main(String[] args) {
         int certCount = 1000;
         int[] certNumbers = new int[certCount];
-        for(int i = 0; i < certNumbers.length; i++){
-            certNumbers[i] = 1_000_000 + (int)Math.round(8_999_999 * Math.random());
+        for (int i = 0; i < certNumbers.length; i++) {
+            certNumbers[i] = 1_000_000 + (int) Math.round(8_999_999 * Math.random());
         }
 
-        for (int i = 0; i < certNumbers.length; i++) {
-            System.out.println(certNumbers[i]);
+        int winnersRate = 100;
+        int[] winnerNumbers = new int[certNumbers.length / winnersRate];
+        int win = 0;
+        for (int i = 0; i < certNumbers.length; i += winnersRate) {
+            winnerNumbers[win++] = certNumbers[i];
+        }
+        for (int i:winnerNumbers) {
+            System.out.println(i);
         }
     }
 }
