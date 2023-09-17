614f812059ef060057b385b5771e4775859796a2
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-17 22:25:48 +0300

    9.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
          int[] floors = {3, 10, 17 , 22, 16, 14};
-        System.out.println(floors[1]);
+
+         floors[1] = 100;
+
+        System.out.println(floors.length);
+         System.out.println(floors[1]);
+         int[] numbers = new int[20];
+
     }
 }
