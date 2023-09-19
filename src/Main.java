5cc6eb74ecce33b9e941e6f524721c53b45461a5
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-19 03:16:34 +0300

    10.8

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Task> taskArrayList = new ArrayList<>() {{
-            add(new Task("Купить молока", "5 пакетов"));
-            add(new Task("Покормить кота", "новым кормом"));
-            add(new Task("Сжечь секретное", "Особенно из синей сумки"));
-        }};
-        for (int i = 0; i < taskArrayList.size(); i++) {
-            System.out.println(taskArrayList.get(i));
+        Integer[] numbers = {10, 20, 30, 40};
+        ArrayList<Integer> values = new ArrayList<>();
+        values.addAll(Arrays.asList(numbers));
+        for (Integer value : values) {
+            System.out.println(value);
         }
-        System.out.println();
-        taskArrayList.add(0,new Task( "Очень срочная задача", ""));
 
-        for (int i = 0; i < taskArrayList.size(); i++) {
-            System.out.println(taskArrayList.get(i));
-        }
-        System.out.println();
+        Integer[] nums = values.toArray(new Integer[0]);
 
-        taskArrayList.remove(1);
-        for (int i = 0; i < taskArrayList.size(); i++) {
-            System.out.println(taskArrayList.get(i));
-        }
-        System.out.println();
-
-        taskArrayList.set(1, new Task("Покормить лошадь","Тоже новым кормом"));
-        for (int i = 0; i < taskArrayList.size(); i++) {
-            System.out.println(taskArrayList.get(i));
-        }
-        System.out.println();
-
+        System.out.println(Arrays.toString(nums));
     }
 }
